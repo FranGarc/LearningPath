a7744573b5f26247ee23d3c748aef0042259f2e5
FranGarc <franciscogarciagarzon@gmail.com>
2024-02-24 12:38:15 +0100

    Fix to prevent calling multiple times the pokemonDetail endpoint

--- app/src/main/java/com/franciscogarciagarzon/learningpath/ui/screens/pokemondetail/PokemonDetail.screen.kt
+++ app/src/main/java/com/franciscogarciagarzon/learningpath/ui/screens/pokemondetail/PokemonDetail.screen.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -29,7 +30,14 @@ fun PokemonDetail(
 
     val pokemonDetailStateFlow by viewModel.uiState.collectAsState()
     lateinit var pokemonDetail: PokemonDetailUi
-    viewModel.getPokemonDetail(pokemonId = id)
+    DisposableEffect(key1 = Unit) {
+        Log.d("PokemonDetail.screen", "DisposableEffect")
+        viewModel.getPokemonDetail(pokemonId = id)
+        onDispose {
+            Log.d("PokemonDetail.screen", "DisposableEffect onDispose")
+        }
+    }
+
 
     val tabs = viewModel.tabs
     val tabIndex = viewModel.tabIndex.collectAsState()
